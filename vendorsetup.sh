a43fa0718ee29a18122394330108daeb87cb50de
sellers86 <kingconner66@gmail.com>
2013-03-31 10:28:59 -0500

    updates. Remove navigation_bar.xml overlay to fix build.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo full_shooter-userdebug
-add_lunch_combo full_shooter-eng
+#add_lunch_combo full_shooter-userdebug
+add_lunch_combo cyanogen_shooter-eng
